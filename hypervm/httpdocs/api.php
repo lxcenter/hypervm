0091889352884d7579a364bb395730d8eed0b745
dterweij <dterweij@a7fcba01-1d1a-41f7-890c-4e3de44aa0df>
2009-09-05 23:15:49 +0000

    Cleanup

--- hypervm/httpdocs/api.php
+++ hypervm/httpdocs/api.php
@@ -1,18 +1,16 @@
 <?php 
-
+//
+// This is just an example how to use the API
+// Feel free to play with it.
+//
 include "htmllib/lib/apilib.phps";
-
 api_main();
-
 function api_main()
 {
-
 	list($server, $port) = explode(":", $_SERVER['SERVER_NAME']);
-
 	print("Add an openvz vps: <br> \n");
 	print("<form method=get action=http://{$_SERVER['SERVER_NAME']}/bin/webcommand.php>"); 
 ?> 
-
 <input type=hidden name=login-class value=client>
 <input type=hidden name=login-name value=admin>
 <input type=hidden name=login-password value=hell>
@@ -26,19 +24,16 @@ Number of Ip addresses <br>
 <input type=text name=v-num_ipaddress_f value=> <br> 
 Contact Email <br> 
 <input type=text name=v-contactemail value=> <br> 
-
 Password<br> 
 <input type=text name=v-password value=> <br> 
 <?php 
-
 	get_and_print_a_select_variable("Ostemplate", "ostemplate_openvz", "v-ostemplate");
 	get_and_print_a_select_variable("Server", "vpspserver_openvz", "v-syncserver");
 	get_and_print_a_select_variable("Plan", "resourceplan", "v-plan_name");
 	?> 
 <input type=submit name=submit value=submit> <br> 
 <?php 
-
-
 }
+?>
 
 
--- hypervm/httpdocs/d.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once "htmllib/lib/include.php"; 
-
-$ret = lxshell_unzip(".", "tmp.tar", null);
-
-dprint("Return: $ret \n");
-
-
