af1c61030b6b76f5ab1e6b9ffdacd69f1860c1d9
dterweij <dterweij@a7fcba01-1d1a-41f7-890c-4e3de44aa0df>
2009-09-06 14:33:12 +0000

    Initial start of creating a HyperVM Installation distribution zip file from SVN.

---
+++ hypervm-install/make-distribution.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# This file creates hypervm-install.zip for distribution from SVN.
+# Please note to server admins, if install-hypervm-master/slave.sh is changed,
+# update them on download server too.
+#
+# - create hypervm-install.zip
+####
