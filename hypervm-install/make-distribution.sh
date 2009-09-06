c8002837ab2a45f0c0e1fa74dcf71f35d7721c23
dterweij <dterweij@a7fcba01-1d1a-41f7-890c-4e3de44aa0df>
2009-09-06 19:24:59 +0000

    Script creating hypervm-install.zip (former program-install.zip) for download server.

--- hypervm-install/make-distribution.sh
+++ hypervm-install/make-distribution.sh
@@ -4,5 +4,20 @@
 # Please note to server admins, if install-hypervm-master/slave.sh is changed,
 # update them on download server too.
 #
-# - create hypervm-install.zip
+####
+echo "##########################################"
+echo "### Creating hypervm-install.zip"
+###
+rm -f hypervm-install.zip
+echo "### ......"
+zip -rq hypervm-install.zip ../hypervm-install -x \
+"../hypervm-install/CVS/*" \
+"../hypervm-install/.svn/*" \
+"../hypervm-install/hypervm-linux/CVS/*" \
+"../hypervm-install/hypervm-linux/.svn/*"
+####
+echo "### hypervm-install.zip created."
+echo "##########################################"
+####
+ls -l hypervm-install.zip
 ####
