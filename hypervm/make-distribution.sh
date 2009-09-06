db604a2381919b3c4696d1399d31fdf53921666b
dterweij <dterweij@a7fcba01-1d1a-41f7-890c-4e3de44aa0df>
2009-09-06 22:00:49 +0000

    make-distribution.sh: 90% ready Makefile: added install: so it copies/moves binaries to proper locations.

--- hypervm/make-distribution.sh
+++ hypervm/make-distribution.sh
@@ -5,6 +5,34 @@
 #
 # - read version
 # - compile c files
-# - create zip (without CVS dirs)
-########
- 
+# - create zip package
+######
+echo "################################"
+echo "### Start packaging"
+echo "### read version..."
+# Read version
+# --> To be created
+# <--
+#
+echo "### Compile c files..."
+# Compile C files
+# Part 1
+cd sbin/console
+make ; make install
+cd ../../
+# Part 2
+cd bin/common
+make ; make install
+cd ../../
+#
+echo "### Create zip package..."
+# Package part
+zip -r9 hypervm-test.zip ./bin ./cexe ./file ./httpdocs ./pscript ./sbin -x \
+"*/CVS/*" \
+"*/.svn/*"
+#
+echo "### Finished"
+echo "################################"
+ls -lh hypervm-*.zip
+#
+
