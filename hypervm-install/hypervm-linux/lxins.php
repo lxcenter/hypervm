4f0b2991be8cec6f6518e681da49e441c3ba7937
dterweij <dterweij@a7fcba01-1d1a-41f7-890c-4e3de44aa0df>
2009-10-08 15:56:04 +0000

    Added AGPL-3.0 License Header

--- hypervm-install/hypervm-linux/lxins.php
+++ hypervm-install/hypervm-linux/lxins.php
@@ -1,3 +1,24 @@
+<?PHP
+//
+//    HyperVM, Server Virtualization GUI for OpenVZ and Xen
+//
+//    Copyright (C) 2000-2009     LxLabs
+//    Copyright (C) 2009          LxCenter
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+?>
+
 <?php
 
 // PHp4, without the lxlabs infrastructure... The code is very bad, primarily because it falls outside the lxlabs framework, and has to work on php4 too, which is something we have no experience in. We need to move as much of the activities to code after the installation of the lxlabs framework.
--- hypervm/bin/common/updatecleanup.sh
+++ hypervm/bin/common/updatecleanup.sh
@@ -1,4 +1,23 @@
-#/bin/sh
+#!/bin/sh
+#
+#    HyperVM, Server Virtualization GUI for OpenVZ and Xen
+#
+#    Copyright (C) 2000-2009     LxLabs
+#    Copyright (C) 2009          LxCenter
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 /bin/cp htmllib/filecore/php.ini /usr/local/lxlabs/ext/php/etc/
 exec /usr/local/lxlabs/ext/php/php ../bin/common/tmpupdatecleanup.php "$@"
 # i am chmoding sbin to 755 inside updatecleanup so needs to do this here.
