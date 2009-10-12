0e2aa49a20ea6dee79d7d236817a96bba5a45405
dterweij <dterweij@a7fcba01-1d1a-41f7-890c-4e3de44aa0df>
2009-10-12 19:19:07 +0000

    Add AGPL-3.0 header

--- hypervm/httpdocs/lang/tr/messagelib.php
+++ hypervm/httpdocs/lang/tr/messagelib.php
@@ -1,3 +1,24 @@
+<?PHP
+//
+//    HyperVM, Server Virtualization GUI for OpenVZ and Xen
+//
+//    Copyright (C) 2000-2009     LxLabs
+//    Copyright (C) 2009          LxCenter
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+?>
+
 <?php
 $__information['updateform_limit_pre'] = " If the quota fields are Left blank, the maximum possible values will be automatically used ";
 $__information['updateform_selfbackupconfig_pre'] = " This is primarily meant to configure the remote backup of the master database and nothing else. The database dump is taken everyday and is saved in a local folder in this machine. If an ftp account is configured here, the file will be uploaded to the machine. This is mainly useful in large cluster setup where the failure of master can have much larger impacts.";
