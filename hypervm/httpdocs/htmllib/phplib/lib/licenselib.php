2cdead66577824f62b11e6b3a040ecdfc9edd452
dterweij <dterweij@a7fcba01-1d1a-41f7-890c-4e3de44aa0df>
2010-06-02 21:19:23 +0000

    * IssueID #257 Set License to Unlimited values.

--- hypervm/httpdocs/htmllib/phplib/lib/licenselib.php
+++ hypervm/httpdocs/htmllib/phplib/lib/licenselib.php
@@ -1,22 +1,16 @@
 <?php 
 
 class licensecom_b extends Lxaclass {
-
-
 static $__desc_nname =  array("", "",  "license");
 static $__desc_lic_ipaddress =  array("n", "",  "Ipaddress");
 static $__desc_clientaccount =  array("", "",  "Client Account");
 static $__desc_password =  array("", "",  "Password");
-
 }
 
 class License extends Lxdb {
-
-
 static $__desc =  array("", "",  "license");
 static $__desc_nname =  array("", "",  "license");
 static $__desc_licensecom_b =  array("", "",  "license");
-
 static $__desc_lic_client_num_f =     array("","",  "number_of_clients");
 static $__desc_lic_pserver_num_f =     array("","",  "number_of_servers");
 static $__desc_lic_maindomain_num_f =     array("","",  "number_of_domains");
@@ -29,10 +23,8 @@ static $__desc_lic_client_f =     array("","",  "client_support");
 static $__desc_lic_node_num_f	 = array("", "",  "number_of_nodes");
 static $__desc_license_upload_f =     array("F","",  "upload_new_license");
 static $__desc_lic_livesupport_name_f =     array("F","",  "username");
-
 static $__acdesc_update_login_info = array("", "",  "login_info");
 static $__acdesc_update_license = array("", "",  "license_update");
-
 
 static function initThisObjectRule($parent, $class, $name = null)
 {
@@ -48,11 +40,13 @@ function updateform($subaction, $param)
 			{
 				$lo = $login->getObject('license');
 				$lic = $login->getObject('license')->licensecom_b;
+				if (isset($lic->lic_ipaddress)) {
 				$vlist['licensecom_b_s_lic_ipaddress'] = array('M', $lic->lic_ipaddress);
-				if (!$lic->lic_pserver_num) {
-					$vlist['__v_button'] = 'Get License From Lxlabs';
+				}
+				if (!isset($lic->lic_pserver_num)) {
+					$vlist['__v_button'] = 'Get License From LxCenter';
 				} else {
-					$vlist['__v_button'] = 'Update License From Lxlabs';
+					$vlist['__v_button'] = 'Update License to unlimited';
 				}
 				if ($login->isAdmin()) {
 					if (!isset($lic->lic_maindomain_num)) {
@@ -61,7 +55,9 @@ function updateform($subaction, $param)
 						$lo->write();
 					}
 					$vlist['lic_pserver_num_f'] = array('M', $lic->lic_pserver_num);
+					if (isset($lic->lic_client_num)) {
 					$vlist['lic_client_num_f'] = array('M', $lic->lic_client_num);
+					}
 					if ($sgbl->isKloxo()) {
 						$vlist['lic_maindomain_num_f'] = array('M', $lic->lic_maindomain_num);
 					} else {
@@ -70,13 +66,16 @@ function updateform($subaction, $param)
 				} else {
 					$vlist['lic_node_num_f'] = array('M', $lic->node_num);
 				}
+				if (isset($lic->lic_livesupport_flag)) {
 				$vlist['lic_live_support_f'] = array('M', $lic->lic_livesupport_flag);
-				//$vlist['lic_ipaddress_f'] = array('M', $lic->lic_ipaddress);
+				}
+				if (isset($lic->lic_client)) {
 				$vlist['lic_client_f'] = array('M', $lic->lic_client);
+				}
+				if (isset($lic->lic_livesupport_name)) {
 				$vlist['lic_livesupport_name_f'] = array('M', $lic->lic_livesupport_name);
-				//$vlist['lic_current_f'] = array('t', lfile_get_contents('__path_program_etc/license.txt'));
+				}
 				return $vlist;
-
 			}
 
 		case "login_info":
@@ -114,52 +113,41 @@ static function doupdateLicense()
 		throw new lxException ("not_admin", '');
 	}
 
-	$lic = $login->getObject('license');
-	//if (!$lic->licensecom_b->clientaccount) {
-		//throw new lxException ("license_login_needed", '');
-	//}
-	//$rmt->login = $lic->licensecom_b->clientaccount;
-	//$rmt->password = $lic->licensecom_b->password;
-
-	//$rmt->ipaddress = $param['licensecom_b_s_lic_ipaddress'];
-
-	//$res = send_to_some_http_server("localhost", "", "5558", $var);
+	$license = $login->getObject('license');
+	$license->parent_clname = $login->getClName();
 
 
-	//$res = unserialize(base64_decode($res));
-	$res = null;
+	$prilist = $login->getQuotaVariableList();
 
-	if (!$res) {
-		$res = send_to_some_http_server("client.lxlabs.com", "", "5558", $var);
-		$res = unserialize(base64_decode($res));
+	foreach($prilist as $k => $v) {
+		if (cse($k, "_flag")) {
+			$login->priv->$k = 'On';
+		} else if (cse($k, "_usage")) {
+			$login->priv->$k = 'Unlimited';
+		} else if (cse($k, "_num")) {
+			$login->priv->$k = 'Unlimited';
+		}
 	}
+	$login->setUpdateSubaction();
+	$login->write();
 
-	if (!$res) {
-		send_mail_to_admin("Could_not_connect_to_license_server", "License has been reset to default");
-		setLicenseTodefault();
-		throw new lxException("could_not_connect_to_license_server", '');
+
+	$lic = $license->licensecom_b;
+	if ($sgbl->isKloxo()) {
+		$def = array("maindomain_num" => "Unlimited", "domain_num" => 'Unlimited', "pserver_num" => "Unlimited", "client_num" => "Unlimited");
+	} else {
+		$def = array("vps_num" => 'Unlimited', "client_num" => "Unlimited");
 	}
-
-	if ($res->exception) {
-		//$exc = new Exception("syncserver:$machine <br> " . $rmt->exception->getMessage());
-		//$res->exception->syncserver = $machine;
-		throw $res->exception;
-		//throw $exc;
+	$list = array("maindomain_num","domain_num","pserver_num");
+	foreach($list as $l) {
+		$licv = "lic_$l";
+		$lic->$licv = $def[$l];
 	}
-
-	$val = trim($res->content);
-
-	if (!$val) {
-		throw new lxException("blank_license", '');
-	}
-
-	decodeAndStoreLicense($res->ipaddress, $val);
-
-	// This is set so that the license alone feature - happens when the license expires - will properly redirect back to the original page. 
-	//$gbl->__this_redirect = '/display.php?frm_action=show';
+	$license->setUpdateSubaction();
+	$license->write();
 }
 
 function isSync() { return false ; }
 
-
 }
+?>
