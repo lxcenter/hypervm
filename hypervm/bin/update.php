3e6b4ce1cf3cfdb210724c904a0ca2547138e9bc
Danny Terweij <d.terweij@lxcenter.org>
2011-11-06 18:04:06 +0100

    Change to GIT version control system.

--- hypervm/bin/update.php
+++ hypervm/bin/update.php
@@ -3,7 +3,7 @@ include_once "htmllib/lib/include.php";
 include_once "lib/updatelib.php";
 include_once "htmllib/lib/updatelib.php";
 
-if (lxfile_exists("CVS") | lxfile_exists("svn") | lxfile_exists("git")) {
+if (lxfile_exists("CVS") || lxfile_exists("svn") || lxfile_exists("git")) {
 	print("Development Version found, just go on.\n");
 //	exit;
 }
